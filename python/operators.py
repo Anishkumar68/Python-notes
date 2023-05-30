45ed3a0497fe1880b381d9f93313c7ada439550c
“anish <anishkumar231999@gmail.com>
2023-05-30 21:44:25 +0530

    added file

---
+++ python/operators.py
@@ -0,0 +1,15 @@
+a = 5 < 4 and 4 > 5
+print(a)
+#  and is show true when both condition are true if there one condition is false so its show false
+
+a = 5 < 6 and 4 > 5 
+print(a)
+
+a = 9 == 6 or 9 == 9 
+print(a)
+# or show to true if there one condition true between two conditions.
+
+a = not 500 > 6000
+print(a)
+
+# its mean 500 is not greater than 6000
